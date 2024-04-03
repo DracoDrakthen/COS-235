b130a58f9bb750bf4920c1786f98744b17099e94
Devon Beal <38815682+DracoDrakthen@users.noreply.github.com>
2024-04-02 21:29:17 -0400

    version 2.0
    Did some fixes to ensure it worked properly

--- Cache.c
+++ Cache.c
@@ -1,3 +1,19 @@
+// Author: Devon Beal
+//
+// Test 1: seemed to have no hits a straight 0% while miss rate was at 100%.
+// This might be due to sequential access to every word. Also since the cahe is initially empty and each new block is empty.
+//
+//Test 2: This one was better once I Fixed it. Hits where about 25% and Miss
+//rate was about 75%. Every word in RAM is accessed in increments of the block 
+//size. This might be due to the cache being small and direct-mapped.
+//
+//Test 3: As put in the veriables the hit and miss rate was dependant on random
+//addresses, and I think I got maybe 1 hit both times I ran this. Since the 
+//addresses coincide with the address already in the cache then the cache hits
+//else does not. 
+//
+//Might need to do some minor changes before submitting  
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -24,8 +40,8 @@ void initialize_cache() {
 
 // Function to simulate cache access
 void access_cache(int address) {
-    int index = (address / WORD_SIZE) % CACHE_SIZE; // Calculate index of cache block
-    int tag = address / (WORD_SIZE * CACHE_SIZE);   // Calculate tag of address
+    int index = (address / WORD_SIZE / BLOCK_SIZE) % CACHE_SIZE; // Calculate index of cache block
+    int tag = address / (WORD_SIZE * BLOCK_SIZE * CACHE_SIZE);   // Calculate tag of address
     
     // Check if the cache block at the calculated index contains the requested data
     if (cache[index].valid && cache[index].tag == tag) {
@@ -45,23 +61,24 @@ int main() {
 
     // Test 1: Sequential access to every word in RAM
     printf("Test 1:\n");
-    int hits = 0, misses = 0;
-    for (int i = 0; i < RAM_SIZE; i++) {
-        access_cache(i * WORD_SIZE);
+    for (int i = 0; i < RAM_SIZE * WORD_SIZE; i += WORD_SIZE) {
+        access_cache(i);
     }
+
+    initialize_cache();
 
     // Test 2: Access every word in RAM in increments of the block size
     printf("\nTest 2:\n");
-    initialize_cache(); // Reset cache for the next test
-    for (int i = 0; i < RAM_SIZE; i += BLOCK_SIZE) {
-        access_cache(i * WORD_SIZE);
+    for (int i = 0; i < RAM_SIZE * WORD_SIZE; i += BLOCK_SIZE * WORD_SIZE) {
+        access_cache(i);
     }
+
+    initialize_cache();
 
     // Test 3: Random access to words in RAM
     printf("\nTest 3:\n");
-    initialize_cache(); // Reset cache for the next test
     for (int i = 0; i < RAM_SIZE; i++) {
-        int random_address = rand() % RAM_SIZE * WORD_SIZE;
+        int random_address = rand() % (RAM_SIZE * WORD_SIZE);
         access_cache(random_address);
     }
 
